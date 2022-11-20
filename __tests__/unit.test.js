50fd13e9c1f9e2f7d4e1a6df5c4017608585a0b5
Averi Yu / Zhizhen Yu <73741014+averi-u@users.noreply.github.com>
2022-11-20 00:19:29 -0800

    Updats: finish half of the Part 2 tests

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,39 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+// const isPhoneNumber = ;
+
+test('isPhoneN_tests', () => {
+    expect(functions.isPhoneNumber(8577574)).toBe(false);
+});
+
+test('isPhoneN_tests', () => {
+    expect(functions.isPhoneNumber(87574)).toBe(false);
+});
+
+test('isEmail_tests', () => {
+    expect(functions.isEmail(87574)).toBe(false);
+});
+
+test('isEmail_tests', () => {
+    expect(functions.isEmail(8237574)).toBe(false);
+});
+
+
+
+
+test('isHexColor_tests', () => {
+    expect(functions.isHexColor(823345347574)).toBe(false);
+});
+
+test('isHexColor_tests', () => {
+    expect(functions.isHexColor('FF0000')).toBe(true);
+});
+
+test('isHexColor_tests', () => {
+    expect(functions.isHexColor('FF0002')).toBe(true);
+});
+
+test('isHexColor_tests', () => {
+    expect(functions.isHexColor('FFsdfs0003')).toBe(false);
+});
